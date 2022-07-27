bea3b4eee0e5f407ecf00e26ebba5349f162e04c
Dima Malik <mdima4266@gmail.com>
2022-07-27 19:52:45 +0300

    Update Fetch.jsx

--- src/components/Fetch/Fetch.jsx
+++ src/components/Fetch/Fetch.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "https://deliveryserverqwe.herokuapp.com";
 
 export const FetchShop = async () => {
   try {
